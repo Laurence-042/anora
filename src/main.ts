986a2e96e46f9173e7680c46d24068b71c65b3e6
Laurence042 <laurence042.mail@gmail.com>
2025-12-09 15:18:17 +0800

    feat: import mods to trigger decorator registration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 导入所有 mods，触发装饰器注册
+import './mods'
+
 const app = createApp(App)
 
 app.use(createPinia())
